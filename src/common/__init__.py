90180832ca2cfa033ddc9453fcbd1a630e0cacd9
hanabi-bro <hanabi.bro@gmail.com>
2026-09-12 21:13:05 +0900

    Initial scaffold for common

---
+++ src/common/__init__.py
@@ -0,0 +1,3 @@
+"""common package."""
+
+__version__ = "0.1.0"
---
+++ src/common/cli.py
@@ -0,0 +1,9 @@
+"""common CLI エントリポイント."""
+
+
+def main() -> None:
+    print("common: hello from common")
+
+
+if __name__ == "__main__":
+    main()
